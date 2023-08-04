14b9b4f4da602cb48c5f1aace32eb56a401a81de
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-04 13:36:36 +0330

    fix a bug

--- apps/HomeApp/models.py
+++ apps/HomeApp/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from jalali_date.utils import jalali_convert
 class ViewHomeModels(models.Model):
 
     view=models.IntegerField()
-    createdDate=models.DateField(auto_now_add=True)
+    createdDate=models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
 
 
-        return f"{self.createdDate}-->{self.view}"
+        return f"{jalali_convert(self.createdDate)}-->{self.view}"

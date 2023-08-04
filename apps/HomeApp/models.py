d050614a5cc25d866acc0de3da627fe4d478f69a
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-04 13:12:45 +0330

    fix a bug

--- apps/HomeApp/models.py
+++ apps/HomeApp/models.py
@@ -3,4 +3,10 @@ from django.db import models
 class ViewHomeModels(models.Model):
 
     view=models.IntegerField()
-    createdDate=models.DateTimeField(auto_now_add=True)
+    createdDate=models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+
+
+        return f"{self.createdDate}-->{self.view}"

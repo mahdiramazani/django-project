c20fe153179e50fcff8e104e31575b4dac40e566
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-06 19:34:10 +0330

    end for day

---
+++ apps/AcountApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView,View
+
+
+class LoginView(TemplateView):
+    template_name = "AcountApp/login.html"
+
+class RegisterView(TemplateView):
+
+    template_name = "AcountApp/register.html"

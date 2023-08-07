d7eefb5369282d1209852bf13605dc651db03388
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-07 21:08:41 +0330

    end for day

--- apps/EventsApp/urls.py
+++ apps/EventsApp/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name="EventsApp"
 urlpatterns=[
     path("",views.EventsView.as_view(),name="EventApp"),
-    path("detail/",views.EventsView.as_view(),name="EventDetail")
+    path("detail/",views.DetailEventView.as_view(),name="EventDetail")
 ]

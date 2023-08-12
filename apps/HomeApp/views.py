f2698fa4fb56584960454b7b9c2cf4cac5893b7a
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-12 20:30:01 +0330

    complate serach

--- apps/HomeApp/views.py
+++ apps/HomeApp/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.shortcuts import render,redirect
+from django.views.generic import TemplateView,View
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from apps.NewsApp.models import NewsModels,ImageGallery,SliderModel
 from apps.GuildRoomApp.models import GuildRoomModel
 from apps.AcountApp.models import User
 from .models import ViewHomeModels
 from django.utils import timezone
-
+from django.db.models import Q
 import datetime
 from datetime import date
 class HomeAppView(TemplateView):
@@ -35,7 +36,24 @@ class HomeAppView(TemplateView):
         context["image_gallery"] = ImageGallery.objects.all()
         context["must_view_news"]=NewsModels.objects.filter(view__gt=10)
         context["slider"]=SliderModel.objects.all()
-        print(context["slider"])
-
 
         return context
+
+
+class SearchView(View):
+
+    def get(self,request):
+        page = request.GET.get('page', 1)
+        q=request.GET.get("q")
+        query=NewsModels.objects.filter(
+            Q(title__icontains=q) or Q(body__in=q)
+        )
+        paginator = Paginator(query, 1)
+        try:
+            object_list = paginator.page(page)
+        except PageNotAnInteger:
+            object_list = paginator.page(1)
+        except EmptyPage:
+            object_list = paginator.page(paginator.num_pages)
+
+        return render(request,"HomeApp/search.html",{"object_list":object_list})

07d9518b9ffc28b84aa3e53b33af648ab0530119
mahdiramazani <mahdiramazani1281@gmail.com>
2023-08-13 19:55:12 +0330

    add change pass to userpanel

--- apps/UserPanelApp/urls.py
+++ apps/UserPanelApp/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path("",views.UserPanelView.as_view(),name="userpanel"),
     path("edit-profile/",views.EditProfile.as_view(),name="edit-panel"),
     path("profile-order/",views.OrderProfileView.as_view(),name="order-profile"),
-    path("order/<int:pk>",views.OrderProfileDetail.as_view(),name="order-detail")
+    path("order/<int:pk>",views.OrderProfileDetail.as_view(),name="order-detail"),
+    path("pass-change/",views.ChangePasswordView.as_view(),name="pass-change")
 ]
--- apps/UserPanelApp/views.py
+++ apps/UserPanelApp/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView,View
 from .forms import EditProfileForm
 from django.urls import reverse
 from apps.PaymentsApps.models import OrderShop
+from apps.AcountApp.models import User
+from django.contrib.auth import login
+
 class UserPanelView(TemplateView):
 
     template_name = "UserPanelApp/profile.html"
@@ -40,3 +43,50 @@ class OrderProfileDetail(View):
         order=OrderShop.objects.get(id=pk)
 
         return render(request,"UserPanelApp/order_detail.html",{"order":order})
+
+
+class ChangePasswordView(View):
+
+    def get(self,request):
+
+
+        return render(request,"UserPanelApp/password-chanage.html")
+
+
+    def post(self,request):
+
+        oldpass=request.POST.get("oldpass")
+        pass1=request.POST.get("pass1")
+        pass2=request.POST.get("pass2")
+        user=User.objects.get(phone=request.user.phone)
+        context={
+            "error":[]
+        }
+
+        if pass1 and pass2 and oldpass:
+
+           if user.check_password(oldpass):
+
+               if pass1 == pass2:
+
+                   user.set_password(pass1)
+                   user.save()
+                   login(request, user)
+
+                   return redirect("UserPanelApp:userpanel")
+
+               else:
+                   context["error"].append("پسورد ها با هم شباهت ندارند")
+                   return render(request, "UserPanelApp/password-chanage.html",context)
+
+           else:
+               context["error"].append("پسورد قدیمی اشتباه است")
+               return render(request, "UserPanelApp/password-chanage.html",context)
+
+        return render(request,"UserPanelApp/password-chanage.html")
+
+
+
+
+
+
